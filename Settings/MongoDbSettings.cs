25e41eb77a43a76a797c20170d5af240fb1ee98f
SIAM <SIAM@DESKTOP-LT1C02P>
2022-04-23 00:35:24 +0600

    Add project files.

---
+++ Settings/MongoDbSettings.cs
@@ -0,0 +1,15 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Play.Catalog.Service.Settings
+{
+    public class MongoDbSettings
+    {
+        public string Host { get; init; }
+        public int Port { get; init; }
+
+        public string ConnectionsString => $"mongodb://{Host}:{Port}";
+    }
+}
